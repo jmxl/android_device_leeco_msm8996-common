c50bcd5e8e754c89dcf18845ba95e40b82869371
shivatejapeddi <shivatejapeddi@gmail.com>
2018-07-20 20:25:07 +0000

    msm8996-common : Add Lights hal to manifest and and build required blobs

--- msm8996.mk
+++ msm8996.mk
@@ -265,6 +265,8 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.hardware.light@2.0 \
     lights.msm8996
 
 # Media
