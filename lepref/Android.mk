13052a2faf9b1e753527c9ba26d27e81d578d00d
Lucchetto <lucchetto.tie@live.com>
2018-07-29 08:54:39 +0200

    Move LePref to vendor
    Change-Id: I1f70966fd1037a156c69bf1ebf0d541711a0361a

--- lepref/Android.mk
+++ lepref/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := LePref
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
