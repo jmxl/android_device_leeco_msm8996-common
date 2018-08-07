8079a738570d99d1eec43aa986c6e0b338f2447f
Lucchetto <lucchetto.tie@live.com>
2018-08-07 20:23:11 +0200

    Fix PowerHAL with VNDK
    Change-Id: Ief6c8162edf86ca9b1680a92166e93afc838b77a

--- power/Android.mk
+++ power/Android.mk
@@ -43,6 +43,8 @@ ifeq ($(TARGET_USES_LAUNCH_BOOST),true)
     LOCAL_CFLAGS += -DLAUNCH_HINTS
 endif
 
+LOCAL_HEADER_LIBRARIES := libhardware_headers
+
 LOCAL_SHARED_LIBRARIES := \
     libbase \
     libcutils \
@@ -51,7 +53,6 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
     android.hardware.power@1.0
-
 
 include $(BUILD_EXECUTABLE)
 endif
